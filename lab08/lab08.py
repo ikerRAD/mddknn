aa7552a68752844e7256ff5d77da5d47acc96df0
ikerRAD <63415322+ikerRAD@users.noreply.github.com>
2021-11-05 22:33:00 +0100

    Add files via upload

--- lab08/lab08.py
+++ lab08/lab08.py
@@ -1,10 +1,8 @@
 import numpy as np
 import pandas as pd
 import statistics as st
-import math
-import csv
 import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold
+from pylab import rcParams
 
 
     
@@ -36,24 +34,31 @@ def main():
     # Separamos las labels del Test. Es como si no nos las dieran!!
     testT = test.loc[:, test.columns != 'mpg']
     testR = test.loc[:, test.columns == 'mpg']
-    
-    print(testT)
-    print(testR)
+
     # Predecimos el conjunto de test
     
     true_labels = 0
     predicted_labels = 0
+    print("\nCasos de test:\n")
     for i in range(len(testT)):
         
         estimado = knn(testT.iloc[i,:], train, 3)
         predicted_labels +=1
-        if estimado == testR[i]:
+        print("prueba ",i,", Clase estimada --> ",estimado," Clase real --> ",testR.iloc[i,0],"\n")
+        if estimado == testR.iloc[i,0]:
             true_labels +=1
 
     # Mostramos por pantalla el Accuracy por ejemplo
     print("Accuracy conseguido:")
     print(accuracy(true_labels, predicted_labels))
 
+    print("\n5-fold cross-validation:")
+    print(kFoldCV(mtcars_normalizado, 5))
+    
+    print("\nleave-one-out cross-validation:")
+    print(kFoldCV(mtcars_normalizado, len(mtcars_normalizado)))
+
+    plots(mtcars)
     # Algun grafico? Libreria matplotlib.pyplot
     return(0)
 
@@ -75,13 +80,55 @@ def splitTrainTest(data, percentajeTrain):
     return(test, train)
 
 def kFoldCV(data, K):
-    """
-    Takes a pandas dataframe and the number of folds of the CV
-    YOU CAN USE THE sklearn KFold function here
-    How to: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.KFold.html
-    """
-
-    return()
+    
+    #we create an array of len(data) values to delimitate the folds
+    if len(data) == K:
+        v = list(range(K))
+    else:
+        #creamos un vector de indices uniformemente distribuidos
+        provisional = list(range(K))
+        mult = int(len(data)/K)
+        provisional = provisional*mult        
+        v = provisional + list(range(len(data)%K))
+        
+    #randomizamos v pero no perdemos elementos
+    np.random.shuffle(v)
+    
+    #hacemos K validaciones
+    tasa = 0
+    for i in range(K):
+        tasatmp = 0
+        totales = 0
+        indexes = []
+        notindexes = []
+        print("Fold-",i+1,":\n")
+        #obtenemos los índices de test y de train
+        for j in range(len(v)):
+            if v[j] == i:
+                indexes.append(j)
+            else:
+                notindexes.append(j)
+                
+        #hacemos test
+        test = data.iloc[indexes,:]
+        train = data.iloc[notindexes,:]
+        
+        testT = test.loc[:, test.columns != 'mpg']
+        testR = test.loc[:, test.columns == 'mpg']
+        
+        #estimamos las muestras de test
+        for t in range(len(testT)):           
+            estimado = knn(testT.iloc[t,:], train, 3)
+            totales +=1
+            print("prueba ",t,", Clase estimada --> ",estimado," Clase real --> ",testR.iloc[t,0],"\n")
+            if estimado == testR.iloc[t,0]:
+                tasatmp +=1
+        
+        #calculamos valores
+        tasa += accuracy(tasatmp, totales)
+        
+    #devolvemos la tasa media
+    return tasa/K
 
 # FUNCION modelo prediccion
 def knn(newx, data, K):
@@ -99,7 +146,8 @@ def knnperform(k, caso, dataset, clases):
     dists = []
     distssorted = []
     #calculamos las distancias
-    for dato in dataset:
+    for i in range(len(dataset)):
+        dato = dataset.iloc[i,:]
         distancia = euclideanDistance2points(caso,dato)
         dists.append(distancia)
         distssorted.append(distancia)
@@ -116,7 +164,9 @@ def knnperform(k, caso, dataset, clases):
         dists[index] = -1
     
     #clases estimadas
-    clasesm = [clases[i] for i in indexes]
+    clasesm = []
+    for i in indexes:
+        clasesm.append(clases.iloc[i,0])
     
     #obtenemos las diferentes clases posibles
     cldif = [clasesm[0]]
@@ -141,7 +191,8 @@ def euclideanDistance2points(x,y):
     
     suma = 0
     for i in range(len(x)):
-        suma = np.power(x.loc[i]-y.loc[i], 2)
+        
+        suma = np.power(x[i]-y[i], 2)
         
     return np.sqrt(suma) 
 
@@ -150,6 +201,31 @@ def euclideanDistance2points(x,y):
 def accuracy(true, pred):
     return true/pred
 
+#FUNCION graficos
+def plots(data):
+    #obtenemos el split de las clases
+    clase0 = data[data['mpg']==0]
+    clase1 = data[data['mpg']==1]
+    
+    rcParams['figure.figsize'] = 15, 20
+    
+    fig,axs = plt.subplots(10,1)
+    fig.suptitle('Features of differen classes')
+    plt.subplots_adjust(left = 0.25, right = 0.9, bottom = 0.1, top = 0.95, wspace = 0.2, hspace = 0.9)
+    
+    for param in range(1,len(data.columns)-1):
+        ax = axs[param-1]
+        clase0[data.columns[param]].plot(kind='density', ax=ax, subplots=True, 
+                                    sharex=False, color="red", legend=True,
+                                    label=data.columns[param] + ' for Outcome = 0')
+        clase1[data.columns[param]].plot(kind='density', ax=ax, subplots=True, 
+                                     sharex=False, color="green", legend=True,
+                                     label=data.columns[param] + ' for Outcome = 1')
+        ax.set_xlabel(data.columns[param+1] + ' values')
+        ax.set_title(data.columns[param+1] + ' density')
+        ax.grid('on')
+    plt.show()
+    fig.savefig('densities.png')
 
 if __name__ == '__main__':
     np.random.seed(25)
